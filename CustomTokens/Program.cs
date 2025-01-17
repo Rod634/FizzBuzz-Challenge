f65ecd8756b437c14554f0ea92bbe0d2269a8450
Rodrigo Conceicao da Silva <rodrigo.silva@blip.ai>
2025-01-17 08:57:53 -0300

    Custom tokens

---
+++ CustomTokens/Program.cs
@@ -0,0 +1,19 @@
+﻿using TwistedFizzBuzzLibrary;
+
+namespace CustomTokens
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customTokens = new Dictionary<string, int>()
+            {
+                { "Fizz", 5 },
+                { "Buzz", 9},
+                { "Bar", 27}
+            };
+
+            TwistedFizzBuzz.AlternaTiveTokens(customTokens, -20, 127);
+        }
+    }
+}
